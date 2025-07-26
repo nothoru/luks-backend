54871c50c4a3775070eb863e2e8ac6ce297f72a9
nothoru <yuiishi022@gmail.com>
2025-07-27 04:50:55 +0800

    startup2

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Install the missing graphics library needed by OpenCV
-apt-get update && apt-get install -y libgl1-mesa-glx
-
+apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
 # Start the Gunicorn server
 gunicorn --bind=0.0.0.0 --timeout 600 backend.wsgi
