4619aa4d99cf1cbb8eb1d1c07174b973896408d8
nothoru <yuiishi022@gmail.com>
2025-08-01 18:20:13 +0800

    rolledback

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "--- Entrypoint script started at $(date) ---"
-
-# --- NEW DEBUGGING BLOCK ---
-echo "--- VERIFYING CRITICAL ENVIRONMENT VARIABLES ---"
-echo "WEBSITE_HOSTNAME is: [${WEBSITE_HOSTNAME}]"
-echo "AZURE_STORAGE_CONNECTION_STRING is: [${AZURE_STORAGE_CONNECTION_STRING}]"
-echo "--- END OF VERIFICATION ---"
-
 echo "--- Entrypoint: Applying database migrations ---"
 python manage.py migrate --noinput
 
